7fa8463e5418923ce7f81b60f928197842761263
Elior Vaisman <elior19v@gmail.com>
2018-01-01 18:17:44 +0200

    requests

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -16,24 +16,24 @@ export class AppComponent {
     // this.http.get("10.160.3.243:8080/Haborer/Rest").subscribe(data=> {console.log(data)});
 
 
-    this.http.post("http://localhost:8080/Haborer-Service/UserService/Squadron/AddItem",
-      {
-        "itemName": "1",
-        "itemCategory": "1",
-        "squadron": "1",
-        "itemMakat":"1"
-      }, this.options)
-      .subscribe(
-        (val) => {
-          console.log("POST call successful value returned in body",
-            val);
-        },
-        response => {
-          console.log("POST call in error", response);
-        },
-        () => {
-          console.log("The POST observable is now completed.");
-        });
+    // this.http.post("http://localhost:8080/Haborer-Service/UserService/Squadron/AddItem",
+    //   {
+    //     "itemName": "1",
+    //     "itemCategory": "1",
+    //     "squadron": "1",
+    //     "itemMakat":"1"
+    //   }, this.options)
+    //   .subscribe(
+    //     (val) => {
+    //       console.log("POST call successful value returned in body",
+    //         val);
+    //     },
+    //     response => {
+    //       console.log("POST call in error", response);
+    //     },
+    //     () => {
+    //       console.log("The POST observable is now completed.");
+    //     });
   }
 }
 
