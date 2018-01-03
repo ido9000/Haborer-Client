7853f81d52f799c0b8ed5b9c6cade2b7e35be85a
Elior Vaisman <elior19v@gmail.com>
2018-01-03 09:56:32 +0200

    dynamic table

--- client/src/app/table/table.component.ts
+++ client/src/app/table/table.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TableComponent implements OnInit {
 
+  items  = [new dummyItem("1","1","1","1","1"),
+            new dummyItem("2","2","2","2","2")];
+
   constructor() { }
 
   ngOnInit() {
@@ -14,6 +17,26 @@ export class TableComponent implements OnInit {
 
   createNewTable(storeContent) {
 
+  }
+
+}
+
+
+
+//dummy data
+export class dummyItem implements itemModule {
+  itemId: string;
+  itemName: string;
+  itemCategory: string;
+  dateAdded: string;
+  squadron: string;
+
+  constructor(itemId, itemName, itemCategory, dateAdded, squadron){
+    this.itemId =itemId;
+    this.itemName = itemName;
+    this.itemCategory = itemCategory;
+    this.dateAdded = dateAdded;
+    this.squadron = squadron;
   }
 
 }
