7d1bed4ced432b6af14c393ab7c3988213b5bbca
עידו <ido.nissan610@gmail.com>
2018-01-03 15:57:20 +0200

    YOYO

--- client/src/app/configuration-file.ts
+++ client/src/app/configuration-file.ts
@@ -2,7 +2,8 @@ import {HttpHeaders} from "@angular/common/http";
 
 export class Configuration {
 
-  serverAddress:string = "localhost:8080/HaborerService/";  //item request factory
+  serverAddress = 'http://localhost:8080/Haborer-Service/';
+
   options = { headers: new HttpHeaders().set('Content-Type', 'text/plain') };
 
   constructor() {
