0339255e66947f05e4ada34026671836e06af2d7
Elior Vaisman <elior19v@gmail.com>
2018-01-06 14:16:01 +0200

    login page

--- client/src/app/current-user.service.ts
+++ client/src/app/current-user.service.ts
@@ -3,6 +3,16 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class CurrentUserService {
 
+  currentUser: userModule;
+
   constructor() { }
+
+  setUser(){
+
+  }
+
+  getUser(){
+
+  }
 
 }
